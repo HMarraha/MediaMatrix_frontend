ea52e6478f2d6764ed48e7c77e6aa9b888ecb77c
HMarraha <marrahahaytam@gmail.com>
2024-04-01 17:22:27 +0200

    started project

---
+++ src/router/router.jsx
@@ -0,0 +1,31 @@
+import {createBrowserRouter, Navigate} from 'react-router-dom'
+import GuestLayout from '../Components/GuestLayout/GuestLayout'
+import Signup from '../Components/GuestLayout/Signup'
+import Login from '../Components/GuestLayout/Login'
+import GuestHome from '../Components/GuestLayout/GuestHome'
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/home"/>
+            },
+            {
+                path:'/home',
+                element: <GuestHome />
+            },
+            {
+                path:'/login',
+                element: <Login />
+            },
+            {
+                path:'/signup',
+                element: <Signup />
+            },
+        ]
+    }
+])
+
+export default router
