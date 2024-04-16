7d3924e9e4b236fb85449f68adf4db852369f17c
Zounoogo <kouritazounoogo442@gmail.com>
2024-04-16 15:30:46 +0100

    signup,login, jsx and scss

--- src/Components/GuestLayout/Login.jsx
+++ src/Components/GuestLayout/Login.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const Login = () => {
+import { useState } from "react";
+
+import "./Login.scss";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Insérer ici la logique de connexion
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-form-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin} className="login-form">
+        <table>
+          <tr>
+            <td>
+              <label htmlFor="username">
+                <h4>Username</h4>
+              </label>
+            </td>
+            <td>
+              {" "}
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="login-input"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="password">
+                <h4>Password</h4>
+              </label>
+            </td>
+            <td>
+              <input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="login-input"
+              />
+            </td>
+          </tr>
+        </table>
 
-export default Login
+        <button type="submit" className="login-button">
+          Connection
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/Components/GuestLayout/Signup.jsx
+++ src/Components/GuestLayout/Signup.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Signup = () => {
+import "./Signup.scss";
+
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Envoyer les données du formulaire à votre backend ou traiter les données ici
+    console.log(formData);
+  };
+
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="signup-form-container">
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit} className="signup-form">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor="username">
+                  <h4>Username</h4>
+                </label>
+              </td>
+              <td>
+                {" "}
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  placeholder="username"
+                  required
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="email">
+                  <h4>Email</h4>
+                </label>
+              </td>
+              <td>
+                {" "}
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="email"
+                  required
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="password">
+                  <h4>Password</h4>
+                </label>
+              </td>
+              <td>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="password"
+                  required
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button type="submit">Inscription</button>
+      </form>
+    </div>
+  );
+};
 
-export default Signup
+export default SignupForm;
